41fab34dbe518c42353014035c5687ffe7ea521d
Drew Knoeller <drewk2629@gmail.com>
2022-03-12 19:17:19 -0500

    working

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-const [name, github] = profileDataArgs;
-
 // console.log(profileDataArgs);
 // const printProfileData = profileDataArr => {
 //    for (let i = 0; i < profileDataArr.length; i++) {
@@ -18,8 +11,27 @@ const [name, github] = profileDataArgs;
 // };
 // printProfileData(profileDataArgs);
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// ============================================================================
 
-    console.log('Portfolio complete check out the index.html to see the output')
-});
+const inquirer = require('inquirer');
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(name, github);
+
+
+// fs.writeFile('./index.html', pageHTML, err => {
+   // if (err) throw err;
+
+   // console.log('Portfolio complete check out the index.html to see the output')
+// });
